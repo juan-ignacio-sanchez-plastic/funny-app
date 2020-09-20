429d824015137294ca0fbc0897e88ecbb94d9583
Juan Sánchez <juansp92@hotmail.com>
2020-09-20 14:56:26 -0300

    feat(app): players variable and pickPlayer() function created
    pickPlayers() pick the name of 2 users and return an object with the name and random HP and DPS

--- src/App.vue
+++ src/App.vue
@@ -3,23 +3,37 @@
 </template>
 
 <script>
+import { randomNumBetween } from "./helperFunctions.js";
+
 export default {
   name: "App",
   data: () => ({
     users: null,
+    players: null,
   }),
   components: {},
   created() {
     this.setUp();
   },
   methods: {
+    randomNumBetween,
     async setUp() {
       await this.getUsers();
+      this.players = [this.pickPlayer(), this.pickPlayer()];
+      console.log(this.players);
     },
     getUsers() {
       return fetch("https://jsonplaceholder.typicode.com/users")
         .then((response) => response.json())
         .then((json) => (this.users = json));
+    },
+    pickPlayer() {
+      const randomIndex = this.randomNumBetween(0, this.users.length - 1);
+      return {
+        name: this.users[randomIndex].name,
+        HP: this.randomNumBetween(-200, -40),
+        DPS: this.randomNumBetween(5, 25),
+      };
     },
   },
 };
